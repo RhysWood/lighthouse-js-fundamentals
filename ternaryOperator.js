05afffbf534663410df94c3e04f1a5f834959cdc
Rhys Wood <rhyswoody@gmail.com>
2021-09-08 22:09:41 -0700

    TernaryOperator Example

---
+++ ternaryOperator.js
@@ -0,0 +1,6 @@
+var eatsPlants = false;
+var eatsAnimals = false;
+
+var category = eatsPlants ? (eatsAnimals ? "omnivore" : "herbivore") : (eatsAnimals ? "carnivore" : "undefined");
+
+console.log(category);
