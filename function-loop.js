a4aceff4baf4c3c233df02dfce6b9ca8ad823ab3
Rhys Wood <rhyswoody@gmail.com>
2021-09-15 02:00:54 +0000

    looping within a functiom

---
+++ function-loop.js
@@ -0,0 +1,9 @@
+let result = "";
+function laugh(num) {
+      for (let i = 0; i < num; i++){
+      result = result + "ha";
+      }
+    result = result + "!";
+    return result;
+}
+console.log(laugh(6));
